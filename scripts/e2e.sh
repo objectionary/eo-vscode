a2154d35824d34a7400a9c1c13467506420bff0f
Sergei Mukhin <sergei_mukhin@outlook.com>
2025-10-11 02:50:26 +0300

    #72 Update scripts

--- scripts/e2e.sh
+++ scripts/e2e.sh
@@ -7,8 +7,11 @@ CODE_TESTS_PATH="$(pwd)/client/out/test"
 CODE_TESTS_WORKSPACE="$(pwd)/client/testFixture"
 export CODE_TESTS_PATH
 export CODE_TESTS_WORKSPACE
-export DISPLAY=:0
 
-bash "$(pwd)/scripts/setTestFixture.sh"
+cd "$(pwd)/server" || return
+npm run build
+
+cd ../
+npm run compile
 
 node "$(pwd)/client/out/end-to-end-test/runTest"
